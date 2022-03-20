5f3f29d717e26f2c4a88fe3e32a7cfdf01c0fabd
formegusto <formegusto@gmail.com>
2022-03-20 16:54:59 +0900

    initial commit

---
+++ src/__init__.py
@@ -0,0 +1,4 @@
+import src.spotify as spotify
+
+__version__ = "1.0.0"
+__all__ = ["spotify"]
---
+++ src/spotify/__init__.py
@@ -0,0 +1,4 @@
+from src.spotify.get_token import get_token
+
+__version__ = "1.0.0"
+__all__ = ["get_token"]
