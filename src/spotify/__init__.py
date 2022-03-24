13987150cf3b4472fbe543daef70b4c61c2754c3
formegusto <formegusto@gmail.com>
2022-03-24 18:37:50 +0900

    update : generate mail box

--- src/spotify/__init__.py
+++ src/spotify/__init__.py
@@ -3,7 +3,8 @@ from src.spotify.search_tracks import search_tracks
 from src.spotify.get_genres import get_genres
 from src.spotify.get_features import get_features
 from src.spotify.get_recommend import get_recommend
+from src.spotify.direct_sel import direct_sel
 
 __version__ = "1.0.0"
 __all__ = ["get_token", "search_tracks",
-           "get_genres", "get_features", "get_recommend"]
+           "get_genres", "get_features", "get_recommend", "direct_sel"]
---
+++ src/spotify/direct_sel.py
@@ -0,0 +1,46 @@
+import requests as req
+from functools import reduce
+from urllib.parse import urlencode
+import pandas as pd
+
+
+def direct_sel(token, id):
+    search_uri = "https://api.spotify.com/v1/tracks/{}".format(id)
+
+    query = urlencode({
+        "market": "KR"
+    })
+
+    headers = {
+        "authorization": "Bearer {}".format(token['access_token'])
+    }
+
+    res = req.get("{}?{}".format(search_uri, query), headers=headers)
+
+    item = res.json()
+
+    _id = item["id"]
+    _name = item["name"]
+
+    artist_list = item['album']['artists']
+    artists = reduce(
+        lambda acc, cur: cur[1]['name'] if cur[0] == 0 else acc +
+        "," + cur[1]['name'],
+        enumerate(artist_list),
+        ""
+    )
+    artists_id = reduce(
+        lambda acc, cur: cur[1]['id'] if cur[0] == 0 else acc +
+        "," + cur[1]['id'],
+        enumerate(artist_list),
+        ""
+    )
+
+    _row = pd.Series({
+        "id": _id,
+        "name": _name,
+        "artists": artists_id,
+        "artists_name": artists
+    })
+
+    return _row
