dd5a722e998a645b83b9eaf704018b7e4af814cd
formegusto <formegusto@gmail.com>
2022-03-26 14:08:32 +0900

    update : seed musics clustering

--- src/utils/visual_norm_data.py
+++ src/utils/visual_norm_data.py
@@ -6,18 +6,30 @@ def visual_norm_data(sel_tracks, norm_features):
     matplotlib.rc('font', family='AppleGothic')
     plt.rcParams['axes.unicode_minus'] = False
 
+    is_general = False
+    if sel_tracks is None:
+        is_general = True
+
     plt.figure(figsize=(16, 8))
     x_ticks_labels = norm_features.columns.difference(['id']).values
 
-    for _idx in range(len(norm_features)):
-        feature = norm_features.iloc[_idx]
-        _id = feature['id']
-        if _id in sel_tracks['id'].values:
-            plt.plot(
-                x_ticks_labels, feature.values[1:], color="g", linewidth=2, label="사용자 음악성향 데이터")
-        else:
-            plt.plot(
-                x_ticks_labels, feature.values[1:], color="g", linewidth=0.15, label="Spotify 추천 데이터")
+    if ~is_general:
+        for _idx in range(len(norm_features)):
+            feature = norm_features.iloc[_idx]
 
-    plt.legend(labels=['사용자 음악성향 데이터', 'Spotify 추천 데이터'], loc='upper right')
+            plt.plot(
+                x_ticks_labels, feature.values[1:], color="g", linewidth=0.15)
+    else:
+        for _idx in range(len(norm_features)):
+            feature = norm_features.iloc[_idx]
+            _id = feature['id']
+            if _id in sel_tracks['id'].values:
+                plt.plot(
+                    x_ticks_labels, feature.values[1:], color="g", linewidth=2, label="사용자 음악성향 데이터")
+            else:
+                plt.plot(
+                    x_ticks_labels, feature.values[1:], color="g", linewidth=0.15, label="Spotify 추천 데이터")
+
+        plt.legend(
+            labels=['사용자 음악성향 데이터', 'Spotify 추천 데이터'], loc='upper right')
     plt.show()
